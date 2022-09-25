a617b78a4d909a02e920783555e39f7944e02a34
Joseph <devanderson0412@gmail.com>
2022-09-25 16:05:20 -0400

    :bugfix

--- components/earnGold/quest.tsx
+++ components/earnGold/quest.tsx
@@ -29,7 +29,7 @@ const DailyQuest = () => {
         <>
             <div className="w-full grid grid-cols-1 gap-5 py-10">
                 {isLoading ? "Fetching Quests..." : <>
-                    {quests.map(quest => <div className="relative w-full bg-[#D24B4B80] border-2 border-white rounded-[16px] py-6 px-9 overflow-hidden">
+                    {quests.map((quest, index) => <div key={index} className="relative w-full bg-[#D24B4B80] border-2 border-white rounded-[16px] py-6 px-9 overflow-hidden">
                         <h1 className="text-white text-[24px] leading-[32px]">{quest.title}</h1>
                         <p className="text-[#B9B8BC] text-[14px] leading-[18px]">{quest.rewardAmount} Gold Available</p>
                         <p className="absolute bg-white rounded-full py-[2px] px-[14px] text-red-700 text-[12px] uppercase right-2 top-1">LIVE</p>
