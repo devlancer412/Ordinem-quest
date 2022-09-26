f1c50ce1bd4d46b9f769c8d3783e8afc27ed09a6
SkelectonDev <wingstonnum1@gmail.com>
2022-09-26 16:50:26 +0900

    Fix NFT card style and Earn gold page

--- pages/earn-gold.tsx
+++ pages/earn-gold.tsx
@@ -4,7 +4,7 @@ import Knights from "components/earnGold/knights";
 import Tweet from "components/earnGold/tweet";
 import { useSolanaNfts } from "hooks/useSolanaNfts";
 import { useTwitterUser } from "hooks/useTwitterUser";
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 
 export default function EarnGold() {
   const tabs = [
@@ -28,18 +28,29 @@ export default function EarnGold() {
   const { currentUser } = useTwitterUser();
   const { allNfts } = useSolanaNfts();
 
+  useEffect(() => {
+    const classList = document.getElementsByClassName("full-body")[0].classList;
+    classList.forEach((el) => {
+      console.log(el);
+      if (el.indexOf("bg-") == 0) {
+        classList.remove(el);
+      }
+    });
+    classList.add("bg-[url('/earn-gold.png')]");
+  }, []);
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 gap-x-8 gap-y-4">
       <Tab.Group defaultIndex={1}>
-        <Tab.List className="flex flex-wrap md:flex-col gap-x-3 gap-y-6 sticky top-10">
+        <Tab.List className="flex flex-nowrap md:flex-col gap-x-3 gap-y-6 sticky top-10">
           {tabs.map((tab, i) => (
             <Tab key={i} as={Fragment}>
               {({ selected }) => (
                 <button
-                  className={`text-white px-5 py-2 rounded-md border border-gray-800 dark:border-gray-400 ${
+                  className={`text-white px-5 py-2 rounded-md border-2 border-white dark:border-white min-w-[145px] ${
                     selected
-                      ? "bg-gradient text-gray-800 dark:text-white"
-                      : "bg-gray-800"
+                      ? "bg-[#ff000033] text-gray-800 dark:text-white"
+                      : "bg-[#26212180]"
                   } ${tab.comingSoon && "pointer-events-none opacity-80"}`}
                 >
                   {tab.title}
@@ -56,7 +67,7 @@ export default function EarnGold() {
         <Tab.Panels className="md:col-span-2 text-white">
           {tabs.map((tab, i) => (
             <Tab.Panel
-              className="bg-[#270318] border border-gray-400 px-5 py-3 rounded-md min-h-[20rem]"
+              className="bg-[#48111180] border-2 border-white px-5 py-3 rounded-md min-h-[20rem]"
               key={i}
             >
               {!currentUser ? (
