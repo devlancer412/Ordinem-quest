75a792ff29048bb03ea2da57d8774dfcb130af6d
Joseph <devanderson0412@gmail.com>
2022-09-27 14:41:56 -0400

    users form db

--- components/state/Global.tsx
+++ components/state/Global.tsx
@@ -1,32 +1,19 @@
 import { useEffect, useState } from "react";
 import { getAllUsers } from "utils/firebase";
 
-const initialData = [{
-    wallet: '5NJV..ky5y',
-    earnedGold: '585,000',
-    solValue: 10,
-    totalRaids: '1,528',
-}, {
-}, {
-}, {
-}, {
-}, {
-}, {
-}, {
-}];
+const formatWalletString = (str: string) => {
+    return str.substring(0, 4) + "..." + str.substring(str.length - 4, str.length);
+}
 
 const GlobalState = () => {
-    const [users, setUsers] = useState<any[]>(initialData);
+    const [users, setUsers] = useState<any[]>([]);
 
-    // useEffect(() => {
-    //     (async () => {
-    //         const users = await getAllUsers();
-    //         console.log(users);
-    //         setUsers(users);
-    //     })
-    // }, []);
-
-    // console.log(users);
+    useEffect(() => {
+        (async () => {
+            const users = await getAllUsers();
+            setUsers(users);
+        })();
+    }, []);
 
     return <div className="global-state w-full h-full py-[23px]">
         <table className="w-full h-full rounded-[20px]">
@@ -43,10 +30,10 @@ const GlobalState = () => {
                 {users?.map((user, index) => (
                     <tr key={index}>
                         <td>{index + 1}</td>
-                        <td>{user?.wallet ?? ''}</td>
-                        <td>{user?.earnedGold ?? ''}</td>
-                        <td>{user?.solValue ?? ''}</td>
-                        <td>{user?.totalRaids ?? ''}</td>
+                        <td>{formatWalletString(user?.wallet ?? '')}</td>
+                        <td>{user?.tokensEarned ?? 0}</td>
+                        <td>{0}</td>
+                        <td>{0}</td>
                     </tr>
                 ))}
             </tbody>
